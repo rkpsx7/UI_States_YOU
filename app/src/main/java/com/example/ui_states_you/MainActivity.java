26cbf26e7c974df74e06c0d20f7ba621204e85c5
rkpsx7 <rkpsx7@gmail.com>
2021-07-03 17:40:52 +0530

    completed

--- app/src/main/java/com/example/ui_states_you/MainActivity.java
+++ app/src/main/java/com/example/ui_states_you/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.ui_states_you;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTvOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvOne = findViewById(R.id.tvOne);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key", "Screen Rotation");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String data = savedInstanceState.getString("key");
+        mTvOne.setText(data);
     }
 }
